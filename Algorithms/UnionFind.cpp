38f6cdacbb11317c2782611336aef9274a244742
Victor Tao <platers81@gmail.com>
2020-04-25 02:37:57 -0400

    Add size to union find

--- Algorithms/UnionFind.cpp
+++ Algorithms/UnionFind.cpp
@@ -3,21 +3,25 @@
 	Complexity O(log n) for Join or Find.
 */
 
-int pai[N];
+int pai[MAXN], sz[MAXN];
 
 void init(int n){
-	for(int i=1; i<=n; i++){
-		pai[i]=i;
+	for(int i = 0; i <= n; i++){
+		pai[i] = i;
+		sz[i] = 1;
 	}
 }
 
 int find(int i){
-	if(pai[i]==i)return i;
-	return pai[i]=find(pai[i]);
+	if(pai[i] == i) return i;
+	return pai[i] = find(pai[i]);
 }
 
 int join(int a, int b){
-	a=find(a);
-	b=find(b);
-	pai[a]=pai[b];
+	a = find(a);
+	b = find(b);
+	if(a != b){
+		sz[b] += sz[a];
+		pai[a]=pai[b];
+	}
 }
